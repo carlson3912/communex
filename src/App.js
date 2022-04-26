d30b529d94c0e5d246b100fc7bfa2f02d4d8cf04
carlson3912 <jackacarlson3912@gmail.com>
2022-04-26 00:51:58 -0700

    final deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar,Footer} from './components'
+import { About, Home,Profile,Item, Srs1, Create,Login,Register, Submit} from './pages'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Srs</h1>
-      </header>
+    <div>
+      <Navbar />
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/srs1" element={<Srs1 />} />
+            <Route path="/" element={<Home />} />
+            <Route path=":item/:id" element={<Item />} />
+            <Route path="/create" element={<Create /> } />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/login" element={ <Login />} />
+            <Route path="/register" element={ <Register />} />
+            <Route path="/submit" element={ <Submit />} />
+          </Routes>
+      <Footer />
     </div>
   );
 }
