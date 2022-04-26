3b7adf2c1ae0d7fa781d9cf91feb14983c8d51ae
carlson3912 <jackacarlson3912@gmail.com>
2022-04-26 15:22:05 -0700

    submission

--- src/pages/submit/Submit.jsx
+++ src/pages/submit/Submit.jsx
@@ -2,41 +2,57 @@ import React from 'react';
 import './submit.css'
 const Submit = () => {
     return(
-    <>
-            <div >
+        <div >
+        <div >
         <h1 id="t"> Submit your art </h1>
         <p1 id ="t">Receive royalty, publicity, and glory.</p1>
         <p id = "s">See our submission standards </p><a id = "l"href ="/">here.</a>
         </div>
-        <form id = "form" action="submission.php" method="POST" class="showForm">
+      <form action="submission.php" >
           <br />
+          Enter your email address:
           <br />
-          <input placeholder="Email:" id = "in" type="email" name="email"></input>
+          <input type="email" name="email" />
           <br />
+          Enter your wallet receive address:
           <br />
-          <input placeholder="Public Wallet:"id = "in" type="text" name="wallet"></input>
+          <input type="text" name="wallet" />
           <br />
+          Enter your design title:
           <br />
-          <input placeholder="Title of your work:" id = "in" type="text" name="designTitle"></input>
+          <input type="text" name="designTitle" />
           <br />
+          Enter your design IPFS URL:
           <br />
-          <input placeholder="IPFS URL:" id = "in" type="text" name="ipfs" ></input>
+          <input type="url" name="ipfs" />
           <br />
           Enter your desired manufacturer:
           <br />
-          <input placeholder="Manufacturer:" id = "in" type="text" name="manufacturer_r" ></input>
+          <input type="radio"  id = "XYZ" name="manufacturer_r" value= '1' />
+          <label for="XYZ" name = "manufacturer_r">SRSDefault</label>
           <br />
           Enter your item id:
           <br />
-          <input placeholder="ItemID:" id = "in" type="" name="itemID" ></input>
+          <input type="radio" name="itemID" value = '1' />
+          <label>Graphic-Tee(1)</label>
+          <input type="radio" name="itemID" value = '2' />
+          <label>Baseball Cap(2)</label>
+          <input type="radio" name="itemID" value = '3' />
           <label>Hoodie(3)</label>  
           <br />
           Enter your royalty:
           <br />
-          <input placeholder="Royalty:" id = "in" type="" name="ipfs" ></input>
-          <input class="second__btn" id = "button_A" type="SUBMIT" value="Submit, and Reap Your Due"></input>
-          </form>
           
-        </>
+          
+          <fieldset class="range__field" />
+            <label >Between 1 and 40 %:</label>
+            <br/>
+            <input type="number" step='1' name="royalty" min="1" max="40" />
+           <br/>
+          <input class="second__btn" id = "button_A" type="SUBMIT" value="Submit, and Reap Your Due"/>
+          <br />
+        </form>
+          </div>
+        
         )};
 export default Submit
